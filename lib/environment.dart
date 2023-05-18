0a810c682f3a3a1f4a9a7b2391ae1796640b6bde
Abdullah Al Foysal <abdullahfoysal32@gmail.com>
2023-05-18 22:36:26 +0600

    clean architecture skeleton added with network config

---
+++ lib/environment.dart
@@ -0,0 +1,10 @@
+bool _isProduction = true;
+Map environment = _isProduction ? prodEnvironment : devEnvironment;
+
+final devEnvironment = {
+  "BASE_URL": "http://139.59.35.127/apex-dmit/public/api/",
+};
+
+final prodEnvironment = {
+  "BASE_URL": "http://139.59.35.127/apex-dmit/public/api/",
+};
---
+++ lib/shared/network/network_constants.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+class NetworkConstants {
+  static const Map<String, String> headers = {
+    HttpHeaders.acceptHeader: "application/json",
+    HttpHeaders.contentTypeHeader: "application/json",
+  };
+
+}
